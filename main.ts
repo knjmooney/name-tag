942184215878f916feeb9c069538b8d438e7cd85
Kevin Mooney <knjmooney@gmail.com>
2020-11-24 13:19:59 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
-input.onPinPressed(TouchPin.P0, function () {
-    basic.showString(name)
-    basic.showString(name)
-    basic.showString(name)
-    name = ""
-    basic.showString("" + (alphabet[current_character_index]))
-})
 input.onButtonPressed(Button.A, function () {
     if (current_character_index < 26) {
         current_character_index += 1
@@ -13,17 +6,24 @@ input.onButtonPressed(Button.A, function () {
     }
     basic.showString(alphabet.charAt(current_character_index))
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showString(name)
+    basic.showString(name)
+    basic.showString(name)
+    name = "My name is "
+    basic.showString(alphabet.charAt(current_character_index))
+})
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
-    basic.pause(100)
+    basic.pause(200)
     name = "" + name + alphabet.charAt(current_character_index)
     current_character_index = 0
-    basic.showString("" + (alphabet[current_character_index]))
+    basic.showString(alphabet.charAt(current_character_index))
 })
 let alphabet = ""
 let name = ""
 let current_character_index = 0
 current_character_index = 0
-name = ""
+name = "My name is "
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 basic.showString(alphabet.charAt(current_character_index))
